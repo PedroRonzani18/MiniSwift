79b53babc24c9c05950b1d05ae6ebd918ef42c3b
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-10-22 18:36:28 -0300

    Random

--- interpreter/expr/ActionExpr.java
+++ interpreter/expr/ActionExpr.java
@@ -1,8 +1,10 @@
 package interpreter.expr;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import error.InternalException;
+import interpreter.type.primitive.FloatType;
 import interpreter.type.primitive.StringType;
 import interpreter.value.Value;
 
@@ -28,7 +30,7 @@ public class ActionExpr extends Expr {
             case Read:
                 return new Value(StringType.instance(), in.nextLine().trim());
             case Random:
-                throw new InternalException("Implement me!");
+                return new Value(FloatType.instance(), (new Random()).nextDouble());
             default:
                 throw new InternalException("Unreachable");
         }
